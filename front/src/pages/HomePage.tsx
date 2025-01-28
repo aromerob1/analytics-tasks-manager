8bfc11f734a13d12d2257cf8554ba385990f2150
aromerob1 <af.romerob1@uniandes.edu.co>
2025-01-28 16:17:19 -0500

    Front initial setup

---
+++ front/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="p-4 bg-white rounded shadow">
+                <h1 className="text-2xl font-bold text-center">Analytics tasks</h1>
+            </div>
+        </div>
+    )
+}
